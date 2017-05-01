7224bc1c21eaeccf8816b7183bdb07f37b9a39ea
locona <locona.0810@gmail.com>
2017-05-02 01:45:16 +0900

    forget

--- main.go
+++ main.go
@@ -101,6 +101,48 @@ type Entities struct {
 		Id          int64
 		Id_str      string
 	}
+	Media []EntityMedia
+}
+
+type EntityMedia struct {
+	Id                   int64
+	Id_str               string
+	Media_url            string
+	Media_url_https      string
+	Url                  string
+	Display_url          string
+	Expanded_url         string
+	Sizes                MediaSizes
+	Source_status_id     int64
+	Source_status_id_str string
+	Type                 string
+	Indices              []int
+	VideoInfo            VideoInfo `json:"video_info"`
+}
+
+type MediaSizes struct {
+	Medium MediaSize `bson:"medium"`
+	Thumb  MediaSize `bson:"thumb"`
+	Small  MediaSize `bson:"small"`
+	Large  MediaSize `bson:"large"`
+}
+
+type VideoInfo struct {
+	AspectRatio    []int     `json:"aspect_ratio"`
+	DurationMillis int64     `json:"duration_millis"`
+	Variants       []Variant `json:"variants"`
+}
+
+type MediaSize struct {
+	W      int    `json:"w"`
+	H      int    `json:"h"`
+	Resize string `json:"resize"`
+}
+
+type Variant struct {
+	Bitrate     int    `json:"bitrate"`
+	ContentType string `json:"content_type"`
+	Url         string `json:"url"`
 }
 
 type Config struct {
